9ab334e844de331baca8c46d537d88d0bea86641
Marek Rusinowski <marekrusinowski@gmail.com>
2015-01-14 02:13:56 +0100

    feat: added pinging from client to server

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ var tlsStream = require("./tlsStream");
 var streamTrans = require("./streamTransformations");
 var Kefir = require("kefir").Kefir;
 
+var SeqGen = {
+    seq: 0,
+    get: function () {
+        SeqGen.seq += 1;
+        return SeqGen.seq;
+    }
+};
+
+var Message = function () {
+    var self = {
+        sSeq: null,
+        cSeq: SeqGen.get(),
+        payload: [],
+        message: ""
+    };
+
+    self.toString = function () {
+        var res = "c" + self.cSeq + " ";
+        if (self.sSeq !== null) {
+            res += "s" + self.sSeq + " ";
+        }
+        res += self.message + "\n";
+        res += self.payload.join(' ') + "\n";
+        return res;
+    };
+
+    return self;
+};
+
 tlsStream = tlsStream.create("localhost", 24948);
 msgStream = streamTrans.toMessages(tlsStream);
 
@@ -17,3 +46,9 @@ msgStream.onEnd(function () {
 msgStream.onValue(function (val) {
     console.log("line: ", val);
 });
+
+setInterval(function () {
+    var msg = Message();
+    msg.message = "ping";
+    tlsStream.emit(msg.toString());
+}, 3000);
