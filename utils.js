ca4d4dd77cc093bc79d6b0cf4f0dde236b790dcb
Marek Rusinowski <marekrusinowski@gmail.com>
2015-01-18 22:58:09 +0100

    feat: encryption and decryption functions

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var NodeRSA = require('node-rsa');
+var crypto = require('crypto');
 
 var rsaOptions = {
     environment: 'node',
@@ -31,7 +32,52 @@ function parseRsaPublic(derBase64) {
     }
 }
 
+function encrypt(myKey, friendKey, text) {
+    try {
+        var key = crypto.randomBytes(256 / 8);
+        var iv = crypto.randomBytes(128 / 8);
+        var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+        cipher.setAutoPadding(true);
+        var encrypted = cipher.update(text, 'utf8');
+        var encryptedEnd = cipher.final();
+        encrypted = Buffer.concat([encrypted, encryptedEnd]);
+
+        return {
+            signature: myKey.sign(encrypted, 'base64'),
+            message: encrypted.toString('base64'),
+            key: friendKey.encrypt(key, 'base64'),
+            iv: iv.toString('base64')
+        };
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+function decrypt(myKey, friendKey, data) {
+    try {
+        var encrypted = new Buffer(data.message, 'base64');
+        var iv = new Buffer(data.iv, 'base64');
+        var key = myKey.decrypt(new Buffer(data.key, 'base64'), 'buffer');
+
+        if (!friendKey.verify(encrypted, data.signature, 'buffer', 'base64')) {
+            throw new Error('signature not valid');
+        }
+
+        var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+        var cleartext = decipher.update(encrypted, null, 'utf8');
+        cleartext += decipher.final('utf8');
+
+        return cleartext;
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
 module.exports = {
     loadRsaKey: loadRsaKey,
-    parseRsaPublic: parseRsaPublic
+    parseRsaPublic: parseRsaPublic,
+    encrypt: encrypt,
+    decrypt: decrypt
 };
